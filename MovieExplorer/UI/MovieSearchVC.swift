1408de9844e88c6215275decbca3483830e52d22
Evgenii Kazakov <azeffin@gmail.com>
2018-09-08 18:58:57 +0300

    chore: project created

---
+++ MovieExplorer/UI/MovieSearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  MovieExplorer
+//
+//  Created by Evgeny Kazakov on 9/8/18.
+//  Copyright © 2018 Evgeny Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
---
+++ MovieExplorerTests/MovieExplorerTests.swift
@@ -0,0 +1,14 @@
+//
+//  MovieExplorerTests.swift
+//  MovieExplorerTests
+//
+//  Created by Evgeny Kazakov on 9/8/18.
+//  Copyright © 2018 Evgeny Kazakov. All rights reserved.
+//
+
+import XCTest
+@testable import MovieExplorer
+
+class MovieExplorerTests: XCTestCase {
+    
+}
