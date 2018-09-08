2c0df268d2fa6974d0c544ed221526215d844c10
Evgeny Kazakov <azeffin@gmail.com>
2018-09-08 18:58:57 +0300

    chore: project created

---
+++ MovieExplorer/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MovieExplorer
+//
+//  Created by Evgeny Kazakov on 9/8/18.
+//  Copyright © 2018 Evgeny Kazakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+
+}
---
+++ MovieExplorer/UI/ExploreVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  MovieExplorer
+//
+//  Created by Evgeny Kazakov on 9/8/18.
+//  Copyright © 2018 Evgeny Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
